234ef54f2d3348d21e47918e715b4110e835f63c
callmeradical <lars@callmeradical.com>
2018-11-20 17:50:10 -0500

    updating my index message for demo

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Developer Experience Demo using EKS and Jenkins X for the win!"
 
 	from := ""
 	if r.URL != nil {
